a0cc45a7249c768dab347e2d71fb444c4db15e1f
Mariano <mmontori@umich.edu>
2018-05-07 17:04:28 -0400

    sprite draw optimizations

--- src/views/sprites.c
+++ src/views/sprites.c
@@ -6,6 +6,8 @@ UINT8 k;
 UINT16 spriteTime;
 
 void drawSprites(State *state){
+	UINT8 xj;
+	UINT8 yj;
 	if(intervalCheck(&spriteTime, 5)){
 		Object* curSprite;
 		UINT8 curFrame;
@@ -14,11 +16,19 @@ void drawSprites(State *state){
 			for(j = 0; j < curSprite->numTiles; ++j){
 				curFrame = curSprite->frameCount;
 				if(curSprite->frames[j]->numFrames == 1) curFrame = 0;
+				if(curSprite->dimension->width == 2){
+					xj = j & 1;
+					yj = j >> 1;
+				}
+				else {
+					xj = j % curSprite->dimension->width;
+					yj = j / curSprite->dimension->width;
+				}
 				set_sprite_tile(j + curSprite->startTile, curSprite->frames[j]->frames[curFrame]);
 				move_sprite(
 					j + curSprite->startTile, 
-					curSprite->dimension->x + 8 * (j % curSprite->dimension->width), 
-					curSprite->dimension->y + 8 * (j / curSprite->dimension->width)
+					curSprite->dimension->x + 8 * xj, 
+					curSprite->dimension->y + 8 * yj
 				);
 			}
 			if(++curSprite->frameCount >= curSprite->maxFrames) curSprite->frameCount = 0;
